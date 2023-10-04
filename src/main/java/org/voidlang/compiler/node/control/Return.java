484d6822320918404e5fef739d2b2b2551d783d8
AdvancedAntiSkid <abel.herfort@gmail.com>
2023-10-04 22:41:40 +0200

    feat(compiler): compare return value to method return type

--- src/main/java/org/voidlang/compiler/node/control/Return.java
+++ src/main/java/org/voidlang/compiler/node/control/Return.java
@@ -3,20 +3,20 @@ package org.voidlang.compiler.node.control;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.Nullable;
-import org.voidlang.compiler.node.Generator;
-import org.voidlang.compiler.node.Node;
-import org.voidlang.compiler.node.NodeInfo;
-import org.voidlang.compiler.node.NodeType;
+import org.voidlang.compiler.node.*;
 import org.voidlang.compiler.node.local.Loadable;
+import org.voidlang.compiler.node.type.core.Type;
+import org.voidlang.compiler.node.type.named.NamedScalarType;
+import org.voidlang.compiler.node.value.Value;
 import org.voidlang.llvm.element.IRBuilder;
 import org.voidlang.llvm.element.IRValue;
 
 @RequiredArgsConstructor
 @Getter
 @NodeInfo(type = NodeType.RETURN)
-public class Return extends Node {
+public class Return extends Instruction {
     @Nullable
-    private final Node value;
+    private final Value value;
 
     /**
      * Generate an LLVM instruction for this node
@@ -25,10 +25,35 @@ public class Return extends Node {
     @Override
     public IRValue generate(Generator generator) {
         IRBuilder builder = generator.getBuilder();
-        if (value != null)
-            return builder.returnValue(value.generateAndLoad(generator));
-        else
+
+        // simply return a void if no value was specified
+        // TODO check if the method actually returns void
+        if (value == null)
             return builder.returnVoid();
+
+        // resolve the type of the value to be returned and the return type of the function
+        Type valueType = value.getValueType();
+        Type returnType = getContext().getReturnType();
+
+        // extract the type of the value
+        if (valueType instanceof NamedScalarType namedValueType)
+            valueType = namedValueType.getScalarType();
+
+        // extract the type of the return type
+        if (returnType instanceof NamedScalarType namedReturnType)
+            returnType = namedReturnType.getScalarType();
+
+        // check if the value does not match the method return type
+        if (!valueType.equals(returnType))
+            throw new IllegalStateException(
+                "Cannot return " + valueType + "(" + valueType.getClass().getSimpleName() + ") in a " + returnType +
+                "(" + returnType.getClass().getSimpleName() + ") function"
+            );
+
+        // generate the code to the value to be returned
+        IRValue value = getValue().generateAndLoad(generator);
+        // let the current block to be terminated, and the value be returned
+        return builder.returnValue(value);
     }
 
     /**
